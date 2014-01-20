7313fc043b12e415d3cc00dc31add1f92ce8c125
Avinothini <vinosharav@gmail.com>
2014-01-20 18:06:30 +0530

    Create sqrt.py
    here for a given value square root is found by importing math,maximum and ceil value is also found

---
+++ sqrt.py
@@ -0,0 +1,6 @@
+import math
+n=float(input("enter number:"))
+#print("square root is", math.sqrt(n))
+#print("ceil value is", math.ceil(n))
+#print("abs value is", abs(n))
+print("max of 70 and number ", max(n,70))
